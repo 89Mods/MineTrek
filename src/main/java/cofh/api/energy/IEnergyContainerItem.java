73674274c2d07b53dfaea20808ea4108f51f93d7
89Mods <luca.horn@gmx.de>
2015-02-20 21:19:04 +0100

    Updated CoFH

--- src/main/java/cofh/api/energy/IEnergyContainerItem.java
+++ src/main/java/cofh/api/energy/IEnergyContainerItem.java
@@ -1,10 +1,10 @@
-package api.cofh.api.energy;
+package cofh.api.energy;
 
 import net.minecraft.item.ItemStack;
 
 /**
  * Implement this interface on Item classes that support external manipulation of their internal energy storages.
- * 
+ * <p>
  * A reference implementation is provided {@link ItemEnergyContainer}.
  * 
  * @author King Lemming
